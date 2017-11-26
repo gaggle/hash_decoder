1b2943e485c1dee1c2bd3129de13853796dc7502
Jon Lauridsen <mail@jonlauridsen.com>
2017-11-26 22:11:36 +0000

    Add log_same_line and log_switch

--- hashdecoder/logutil.py
+++ hashdecoder/logutil.py
@@ -1,5 +1,13 @@
 from contextlib import contextmanager
-from typing import Any, Iterator
+from logging import DEBUG, INFO, getLogger
+from typing import Any, Callable, Iterator, TYPE_CHECKING
+
+import sys
+
+if TYPE_CHECKING:
+    import logging
+
+log = getLogger(__name__)
 
 
 @contextmanager
@@ -13,6 +21,7 @@ def log_ctx(msg: str, *args: Any,
         yield
     else:
         print(msg % args + '... ', end='')
+        sys.stdout.flush()
         try:
             yield
         except Exception:
@@ -20,3 +29,27 @@ def log_ctx(msg: str, *args: Any,
             raise
         else:
             print('OK')
+
+
+_LOG_MAX_LENGTH = {}
+
+
+def log_same_line(msg: str, *args: Any) -> None:
+    full_msg = msg % args
+    if len(full_msg) > _LOG_MAX_LENGTH.get(None, 0):
+        _LOG_MAX_LENGTH[None] = len(full_msg)
+
+    max_length = _LOG_MAX_LENGTH[None]
+    padding = max_length - len(full_msg)
+    if padding > 0:
+        full_msg = full_msg + " " * padding
+    print(full_msg, end="\r")
+
+
+def log_switch(logger: 'logging.Logger',
+               debug: Callable,
+               info: Callable) -> None:
+    if logger.isEnabledFor(DEBUG):
+        return debug()
+    if logger.isEnabledFor(INFO):
+        return info()
